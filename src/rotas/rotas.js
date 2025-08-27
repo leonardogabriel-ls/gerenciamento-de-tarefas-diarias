3f0a43db2c6f4e09598bccbdadb943f0053da9bf
lorde-kaphwan <leonardo.gabriel.27699@gmail.com>
2025-08-27 18:27:28 -0300

    novas rotas

--- src/rotas/rotas.js
+++ src/rotas/rotas.js
@@ -5,8 +5,25 @@ rotas.get('/', (req, res) => {
     res.send("bem  bbbb")
 })
 
+rotas.post('/tarefas', (req, res) => {
+    res.send("criar tarefas")
+})
 
+rotas.get('/tarefas', (req, res) => {
+    res.send("listar as tarefas")
+})
 
+rotas.get('/tarefas/:id', (req, res) => {
+    res.send(`lista uma tarefa com o id ${req.params.id}`)
+})
+
+rotas.put('/tarefas/:id', (req, res) => {
+    res.send(`tarefas atualizada`)
+})
+
+rotas.delete('/deletar/:id', (req, res) => {
+    res.send(`tarefa deletada`)
+})
 module.exports = {
     rotas
 }
